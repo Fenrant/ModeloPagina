56fe1edb70e7c093a9869f2c0483d0b049d944f2
Inés Fajardo Romero <ines.fajardo@horizontesazules.ec>
2022-07-28 19:16:52 -0500

    Proyecto inicial modelo conversacional

---
+++ app.py
@@ -0,0 +1,125 @@
+
+from flask import Flask, request, render_template
+from joblib import load
+import pandas as pd
+import numpy as np
+import re
+import string
+from sklearn.metrics import accuracy_score
+import pickle
+import nltk
+import neattext as nt
+import neattext.functions as nfx
+from nltk.corpus import stopwords
+from clases import variablesInicio
+import random
+
+nltk.download('stopwords')
+nltk.download('punkt')
+
+app = Flask(__name__, template_folder="templates")
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+
+@app.route('/prediccion', methods=['GET'])
+def prediction():
+    return render_template('prediction.html')
+
+
+@app.route('/clasificadorModelo', methods=['POST'])
+def clasificador():
+
+    try:
+        body = request.get_json()
+        validarRequestBody(body)
+        variablesInicio.definirVariablesModelo(body['modelo'], body['dataset'])
+
+        entrada = procesarEntrada(body['entrada'])
+        x_entry = vectorizarEntrada(entrada)
+        y_predict = clasificar(x_entry)
+        columnas, salidas = obtenerResultados(y_predict)
+
+        res = {"columnas": columnas, "entrada": body['entrada'],
+               "salidaModelo": y_predict[0].tolist(), "prediccion": salidas}
+
+        return {"codigo": 0, "mensaje": "Operacion Realizada con éxito", "Respuesta": res}
+    except ValueError as e:
+        return {"codigo": random.randint(1, 100), "mensaje": str(e), "body": None}
+
+
+def validarRequestBody(body):
+
+    if body:
+        if "modelo" not in body or body['modelo'] == '':
+            raise ValueError('Se debe especificar el modelo')
+        if "dataset" not in body or body['dataset'] == '':
+            raise ValueError('se debe especificar el dataset')
+        if "entrada" not in body or body['entrada'] == '':
+            raise ValueError('se debe incluir una entrada')
+
+        if body['modelo'] not in variablesInicio.modelos:
+            raise ValueError(
+                'se debe especificar un modelo válido: Randosm Forest(rf) o Support vector Machine(svm)')
+        if body['dataset'] not in variablesInicio.datasets:
+            raise ValueError(
+                'se debe especificar un dataset válido: sintomas o recomendaciones')
+    else:
+        raise ValueError('No existe un body Request')
+
+
+def procesarEntrada(entrada):
+    entrada_limpia = clean_text(entrada)
+    entrada_separada_espacios = entrada_limpia.split(" ")
+    important_words = filter(lambda x: x not in stopwords.words(
+        'spanish'), entrada_separada_espacios)
+    entry = " ".join(list(important_words))
+    return entry
+
+
+def vectorizarEntrada(entrada):
+    entrada_vectorizada = variablesInicio.vectorizador.transform(
+        [entrada]).toarray()
+    return entrada_vectorizada
+
+
+def clasificar(entrada_vectorizada):
+    y_predict = variablesInicio.clasificador.predict(entrada_vectorizada)
+    return y_predict
+
+
+def obtenerResultados(y_predict):
+    columnas = []
+    salida = []
+
+    for columna, valor in zip(variablesInicio.y_test, y_predict[0]):
+        columnas.append(columna)
+        if valor == 1:
+            salida.append(columna)
+
+    return columnas, salida
+
+
+def clean_text(text):
+    text = str(text)
+    text = text.lower()
+    text = re.sub('á', 'a', text)
+    text = re.sub('é', 'e', text)
+    text = re.sub('í', 'i', text)
+    text = re.sub('ó', 'o', text)
+    text = re.sub('ú', 'u', text)
+    text = re.sub('\[.*?¿\]\%', ' ', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), ' ', text)
+    text = re.sub('\w*\d\w*', '', text)
+    text = re.sub('[‘’“”…«»]', '', text)
+    text = re.sub('\n', ' ', text)
+    text = re.sub(r'\s+', ' ', text)
+    text = re.sub('[^a-zA-Z]', ' ', text)
+    return text
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ clases/variablesInicio.py
@@ -0,0 +1,48 @@
+import pickle
+from joblib import  load
+import pandas as pd
+
+modelos = ['svm','rf']
+datasets = ['sintomas', 'recomendaciones']
+
+clasificador = None
+vectorizador = None
+y_test = None
+
+clf_svm_sintomas = load('sintomas\svm\SVM.joblib')
+svm_sintomas_y_test = pd.read_excel('sintomas\svm\SVM-y_test.xlsx')
+svm_sintomas_vectorizador = pickle.load(open("sintomas\svm\SVM-vectorizador.pickel", "rb"))
+
+clf_rf_sintomas = load('sintomas\\rf\RandomForest.joblib')
+rf_sintomas_y_test = pd.read_excel('sintomas\\rf\RandomForest-y_test.xlsx')
+rf_sintomas_vectorizador = pickle.load(open("sintomas\\rf\RandomForest-vectorizador.pickel", "rb"))
+
+clf_svm_recomendaciones = load('recomendaciones\svm\SVM-Recomendaciones.joblib')
+svm_recomendaciones_y_test = pd.read_excel('recomendaciones\svm\SVM-Recomendaciones-y_test.xlsx')
+svm_recomendaciones_vectorizador = pickle.load(open("recomendaciones\svm\SVM-Recomendaciones.pickle", "rb"))
+
+clf_rf_recomendaciones = load('recomendaciones\\rf\RandomForest-Recomendaciones.joblib')
+rf_recomendaciones_y_test = pd.read_excel('recomendaciones\\rf\RandomForest-Recomendaciones-y_test.xlsx')
+rf_recomendaciones_vectorizador = pickle.load(open("recomendaciones\\rf\RandomForest-Recomendaciones.pickle", "rb"))
+
+def definirVariablesModelo(modelo,dataset):
+
+    if dataset == 'sintomas':
+        global clasificador,vectorizador,y_test
+        if modelo == 'rf':
+            clasificador = clf_rf_sintomas
+            vectorizador = rf_sintomas_vectorizador
+            y_test = rf_sintomas_y_test
+        elif modelo == 'svm':
+            clasificador = clf_svm_sintomas
+            vectorizador = svm_sintomas_vectorizador
+            y_test = svm_sintomas_y_test
+    elif dataset == 'recomendaciones':
+        if modelo == 'rf':
+            clasificador = clf_rf_recomendaciones
+            vectorizador = rf_recomendaciones_vectorizador
+            y_test = rf_recomendaciones_y_test
+        elif modelo == 'svm':
+            clasificador = clf_svm_recomendaciones
+            vectorizador = svm_recomendaciones_vectorizador
+            y_test = svm_recomendaciones_y_test
