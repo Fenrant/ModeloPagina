6b25344b91efbfedbe102cc619dfe133085bdccf
Inés Fajardo Romero <ines.fajardo@horizontesazules.ec>
2022-08-01 05:33:37 -0500

    sintomas y recomendaciones

--- static/js/apis.js
+++ static/js/apis.js
@@ -1,6 +1,6 @@
-function clasificadorModelo(modelo, entrada) {
-    var headers =  { 'Content-Type': 'application/json'};
-    var  data = JSON.stringify({modelo:"rf",dataset:"sintomas", entrada:"tengo fiebre y dolor de cabeza"})
+function clasificadorModelo(modelo, dataset) {
+    var entrada = document.getElementById("myTextarea").value;
+    var  data = JSON.stringify({modelo, dataset, entrada})
     $.ajax({
         type:"POST", 
         url:"http://localhost:5000/clasificadorModelo",
@@ -8,15 +8,40 @@ function clasificadorModelo(modelo, entrada) {
         contentType: "application/json",
         dataType: "json", 
         success:function(datos){ 
-            console.log(datos)
+            var sintomas;
             for(var i = 0; i < datos.Respuesta.columnas.length; i++)
             {
                 var columna = datos.Respuesta.columnas[i];
                 var salida = datos.Respuesta.salidaModelo[i]
-                
-                
-                console.log(columna, salida)
+                var html = `
+                <div class="col-lg-3 col-md-12 col-sm-12 content-column">
+                    <div class="project-block-four">
+                        <div class="inner-box">
+                            <figure class="image-box">
+                                <img src="http://localhost:5000/static/images/clasificacionModelo/${columna}.jpg" alt="">
+                                <div style="
+                                    position: absolute;
+                                    left: 20px;
+                                    top: 20px;
+                                    width: 64px;
+                                    border-radius: 50%;
+                                    z-index: 1;
+                                "><img src="http://localhost:5000/static/images/icons/${salida === 1 ? "success": "error"}.png" alt=""></div>
+                            </figure>
+                            <div class="text">
+                                <h5>${columna.charAt(0).toUpperCase() + columna.toLowerCase().slice(1)}</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+                if(i == 0){
+                    sintomas = html;
+                }else{
+                    sintomas +=html
+                }
+               
             }
-         },
+            document.getElementById("sintomas").innerHTML = sintomas;
+        },
     })
   }
